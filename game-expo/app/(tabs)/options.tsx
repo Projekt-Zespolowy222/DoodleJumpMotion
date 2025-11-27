e38809104ae671309496c1273017a19eca0fd508
alex2545idk <128039946+alex2545idk@users.noreply.github.com>
2025-11-27 16:12:07 +0100

    Dodanie ekranu Options

---
+++ game-expo/app/(tabs)/options.tsx
@@ -0,0 +1,7 @@
+// app/(tabs)/options.tsx
+import React from "react";
+import { OptionsScreen } from "@/src/screens/Options";
+
+export default function OptionsTab() {
+  return <OptionsScreen />;
+}
